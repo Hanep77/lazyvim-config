97916befdf501af080c26299442eee72cef8d7b6
hanep <yudishan26@gmail.com>
2024-07-01 14:03:11 +0700

    hmm

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "rebelot/kanagawa.nvim" },
+  { "https://github.com/sainnhe/gruvbox-material" },
+  { "navarasu/onedark.nvim" },
+  { "projekt0n/github-nvim-theme" },
+  { "ellisonleao/gruvbox.nvim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
